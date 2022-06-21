6e7c6b337a0c852ec8b693c3c8a592ba3a705bc7
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2022-06-22 00:26:12 +0300

    remix

--- src/components/Form/InputContacts.jsx
+++ src/components/Form/InputContacts.jsx
@@ -31,7 +31,7 @@ const InputContacts = ({
   const handleEmail = (e) => {
     setEmail(e.target.value)
     // eslint-disable-next-line
-    const email = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/
+    const email = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
     if(!email.test(String(e.target.value).toLowerCase())){
      setEmailError('Incorrect e-mail!')
     }else{
@@ -42,7 +42,7 @@ const InputContacts = ({
   const handlePhone = (e) => {
     setPhone(e.target.value)
      // eslint-disable-next-line
-    const phone = /^[\+]{0,1}380([0-9]{9})$/
+    const phone = /^[\+]{0,1}380([0-9]{9})$/;
     if(!phone.test(Number(e.target.value))){
       setPhoneError('Phone is incorrect')
     }else{
